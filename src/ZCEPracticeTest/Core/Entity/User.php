2d1385ebcfea3d43864ca29d22160ccf99741feb
Alcalyn <doubjulien@hotmail.fr>
2014-12-05 12:42:41 +0100

    export orm mapping to yaml files

---
+++ src/ZCEPracticeTest/Core/Entity/User.php
@@ -0,0 +1,145 @@
+<?php
+
+/**
+ * PHP version 5.5
+ *
+ * @category Entity
+ * @package  FrontBundle
+ * @author   Julien Maulny <jmaulny@darkmira.fr>
+ * @license  Darkmira <darkmira@darkmira.fr>
+ * @link     www.darkmira.fr
+ */
+namespace ZCEPracticeTest\Core\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * User entity
+ *
+ * @category Entity
+ * @package  Core
+ * @author   Julien Maulny <jmaulny@darkmira.fr>
+ * @license  Darkmira <darkmira@darkmira.fr>
+ * @link     www.darkmira.fr
+ */
+class User
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *      max="63"
+     * )
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     * 
+     * @Assert\Length(
+     *      max="63"
+     * )
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @Assert\Length(
+     *      max="127"
+     * )
+     */
+    private $email;
+    
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
