ee7cb280a1cdd13bc3ce9d954ba18e51bbcb2e6b
Alcalyn <doubjulien@hotmail.fr>
2014-12-22 19:05:57 +0100

    build quiz js

--- web/js/main.js
+++ web/js/main.js
@@ -1,22 +1,22 @@
 $(function () {
-    
+    bindStartButton();
 });
 
 var ZCEApi =
 {
     basepath: $('#js-vars').data('basepath')+'/index.php/',
     
-    createSession: function ()
+    createSession: function (callback)
     {
         $.ajax({
             type: 'POST',
             url: ZCEApi.basepath+'api/session'
         }).done(function (r) {
-            console.log(r);
+            callback && callback(r);
         });
     },
     
-    getQuiz: function (quizId)
+    getQuiz: function (quizId, callback)
     {
         var data = {
             quizId: quizId
@@ -27,7 +27,52 @@ var ZCEApi =
             url: ZCEApi.basepath+'api/questions',
             data: data
         }).done(function (r) {
-            console.log(r);
+            callback && callback(r);
         });
     }
 };
+
+function bindStartButton() {
+    if ($('#create-session').size()) {
+        $('#create-session').click(function () {
+            createAndRunSession();
+        });
+    }
+}
+
+function createAndRunSession() {
+    ZCEApi.createSession(function (data) {
+        console.log('data', data);
+        var quiz = createQuiz(data);
+    });
+}
+
+function createQuiz(data) {
+    var questions = [];
+    
+    $.each(data.quiz.quizQuestions, function (i, questionData) {
+        var question = questionData.question;
+        var answers = [];
+        
+        $.each(question.questionQCMChoices, function (j, questionQCMChoice) {
+            answers.push({
+                text: questionQCMChoice.entitled,
+                correct: questionQCMChoice.isValid
+            });
+        });
+        
+        questions.push({
+            text: question.entitled,
+            type: (question.nbAnswers > 1) ? 'checkbox' : 'radio',
+            answers: answers
+        });
+    });
+    
+    var quiz = {
+        title: 'Title',
+        description: 'Description',
+        questions: questions
+    };
+    
+    return quiz;
+}
