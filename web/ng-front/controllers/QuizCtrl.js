0235a42ddcd36f20435a42683a2bb844b366edf3
Alcalyn <doubjulien@hotmail.fr>
2015-01-30 23:10:35 +0100

    [#12] persist quiz data and position, load persisted data on quiz reload, fix bug with radios

--- web/ng-front/controllers/QuizCtrl.js
+++ web/ng-front/controllers/QuizCtrl.js
@@ -3,9 +3,13 @@ zcpe.controller('QuizCtrl', ['$scope', '$location', '$localStorage', '$controlle
     
     $controller('QuizzCtrl', {$scope: $scope});
     
+    $scope.questionChange = function () {
+        saveQuiz();
+    };
+    
+    var parentFinish = $scope.finish;
     $scope.finish = function () {
-        $scope.currentQuestion = null;
-        $scope.finished = true;
+        parentFinish();
         
         var score = evaluator.evaluate($scope.quizz);
         
@@ -14,9 +18,54 @@ zcpe.controller('QuizCtrl', ['$scope', '$location', '$localStorage', '$controlle
         
         $localStorage.score = score;
         $localStorage.sessionData = null;
+        $localStorage.hasStoredQuiz = false;
+        $localStorage.quizz = null;
+        $localStorage.quizPosition = null;
         
         $location.path('/session-result');
     };
+    
+    /**
+     * Persist user answers in local storage
+     */
+    function saveQuiz() {
+        $localStorage.hasStoredQuiz = true;
+        $localStorage.quizz = $scope.quizz;
+        $localStorage.quizPosition = $scope.questionIndex();
+    }
+    
+    /**
+     * Load user answers from local storage
+     */
+    function loadQuiz() {
+        $scope.quizz = $localStorage.quizz;
+        $scope.currentQuestion = $scope.quizz.questions[$localStorage.quizPosition];
+        
+        refreshRadioAnswers($scope.quizz);
+    }
+    
+    /**
+     * Fix bug, set same answer object for radios
+     * 
+     * @param {Object} quizz
+     */
+    function refreshRadioAnswers(quizz) {
+        angular.forEach(quizz.questions, function (question) {
+            if ('radio' === question.type) {
+                if (question.selectedAnswer) {
+                    var i = 0;
+                    var count = question.answers.length;
+                    for (i = 0; i < count; i++) {
+                        if (question.answers[i].id === question.selectedAnswer.id) {
+                            question.selectedAnswer = question.answers[i];
+                        }
+                    }
+                }
+            }
+        });
+    }
+    
+    $scope.loadQuiz = loadQuiz;
     
     $scope.timerFinished = function () {
         $scope.finish();
@@ -24,4 +73,8 @@ zcpe.controller('QuizCtrl', ['$scope', '$location', '$localStorage', '$controlle
     
     $scope.init(quizCreator.createQuiz(sessionData));
     $scope.start();
+    
+    if ($localStorage.hasStoredQuiz) {
+        loadQuiz();
+    }
 }]);
